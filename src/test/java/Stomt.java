9cddc0b464f6eb0622a882ce2485d4903e8abe56
Max Klenk <max@klenk.biz>
2015-10-09 13:32:01 +0200

    comment out old tests (should be removed)

--- src/test/java/Stomt.java
+++ src/test/java/Stomt.java
@@ -10,7 +10,7 @@ public class Stomt {
 	
 	public void createStomt(StomtClient client) throws ParseException, IOException, StomtException {
 		
-		System.out.println(client.createStomt("stomt", true, "would ABCDEFGH1111111", null, true, null, null));
+		//System.out.println(client.createStomt("stomt", true, "would ABCDEFGH1111111", null, true, null, null));
 		
 		
 		//{"anonym":false,"negative":true,"prefetched":false,"text":"would abcdefgh1234","creator":{"id":"test1337","displayname":"test1337","category":{"id":"users","displayname":"Users"},"images":{"avatar":{"url":"https://test.rest.stomt.com/placeholders/30/4.png","w":30,"h":42}},"ownedTargets":[],"roles":[],"lang":"en","ownedTargetsArray":[]},"target_id":"stomt"}
