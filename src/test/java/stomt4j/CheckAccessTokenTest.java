37f4ea1bf6739feebdaf4249dad4018923285ffa
Max Klenk <max@klenk.biz>
2015-10-09 13:56:30 +0200

    comment out unimplemented test

--- src/test/java/stomt4j/CheckAccessTokenTest.java
+++ src/test/java/stomt4j/CheckAccessTokenTest.java
@@ -6,9 +6,9 @@ import org.junit.Test;
 
 public class CheckAccessTokenTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+//	@Test
+//	public void test() {
+//		fail("Not yet implemented");
+//	}
 
 }
